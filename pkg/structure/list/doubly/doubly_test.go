354c5a9c5e3f7b582f5af5e93ff987ee80992889
Archit <archit5793@gmail.com>
2021-01-03 17:30:36 -0500

    add doubly test

---
+++ pkg/structure/list/doubly/doubly_test.go
@@ -0,0 +1,33 @@
+package doubly
+
+import (
+	"math/rand"
+	"testing"
+)
+
+func TestNewNode(t *testing.T) {
+	var num int = rand.Int()
+	var newNode *Node = NewNode(num)
+	if newNode.value != num {
+		t.Errorf("Expected NewNode(%v) value to equal %v", num, newNode.value)
+	}
+	if newNode.next != nil {
+		t.Errorf("Expected NewNode(%v) next to equal %v", num, newNode.next)
+	}
+	if newNode.previous != nil {
+		t.Errorf("Expected NewNode(%v) previous to equal %v", num, newNode.previous)
+	}
+}
+
+func TestNewLinkedList(t *testing.T) {
+	var newList LinkedList = NewLinkedList()
+	if newList.head != nil {
+		t.Errorf("Expected NewLinkedList() head nil to equal %v", newList.head)
+	}
+	if newList.tail != nil {
+		t.Errorf("Expected NewLinkedList() tail nil to equal %v", newList.tail)
+	}
+	if newList.length != 0 {
+		t.Errorf("Expected NewLinkedList() length 0 to equal %v", newList.length)
+	}
+}
