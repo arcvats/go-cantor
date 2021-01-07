5dcf687eeb5d4867a03dcc6e31fde6913cd12cbf
Archit <archit5793@gmail.com>
2021-01-06 22:57:51 -0500

    add tests doubly

--- pkg/structure/list/doubly/doubly_test.go
+++ pkg/structure/list/doubly/doubly_test.go
@@ -1,6 +1,7 @@
 package doubly
 
 import (
+	"github.com/stretchr/testify/assert"
 	"math/rand"
 	"testing"
 )
@@ -8,26 +9,14 @@ import (
 func TestNewNode(t *testing.T) {
 	num := rand.Int()
 	newNode := NewNode(num)
-	if newNode.value != num {
-		t.Errorf("Expected NewNode(%v) value to equal %v", num, newNode.value)
-	}
-	if newNode.next != nil {
-		t.Errorf("Expected NewNode(%v) next to equal %v", num, newNode.next)
-	}
-	if newNode.previous != nil {
-		t.Errorf("Expected NewNode(%v) previous to equal %v", num, newNode.previous)
-	}
+	assert.Equal(t, newNode.value, num, "should be equal")
+	assert.Nil(t, newNode.next, "next should be nil")
+	assert.Nil(t, newNode.previous, "previous should be nil")
 }
 
 func TestNewLinkedList(t *testing.T) {
 	newList := NewLinkedList()
-	if newList.head != nil {
-		t.Errorf("Expected NewLinkedList() head nil to equal %v", newList.head)
-	}
-	if newList.tail != nil {
-		t.Errorf("Expected NewLinkedList() tail nil to equal %v", newList.tail)
-	}
-	if newList.length != 0 {
-		t.Errorf("Expected NewLinkedList() length 0 to equal %v", newList.length)
-	}
+	assert.Nil(t, newList.head, "head should be nil")
+	assert.Nil(t, newList.tail, "tail should be nil")
+	assert.Zero(t, newList.length, "length should be 0")
 }
